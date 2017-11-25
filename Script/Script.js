24d95016a9235ed046f9a09a8df634852577544d
mryelisse <maryelissegonzales@gmail.com>
2017-11-25 11:35:22 +0800

    Updated the design

--- Script/Script.js
+++ Script/Script.js
@@ -18,6 +18,80 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     var pos = $id.offset().top;
 
     // animated top scrolling
-    $('body, html').animate({scrollTop: pos}, 1700);
+    $('body, html').animate({scrollTop: pos}, 1000);
 				
 });
+
+
+//Sliders
+$("#swimmingContainer > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#swimmingContainer > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#swimmingContainer');
+},  3000);
+
+
+$("#massageContainer > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#massageContainer > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#massageContainer');
+},  3000);
+
+
+$("#nightContainer > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#nightContainer > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#nightContainer');
+},  3000);
+
+
+
+//enlarge when hover
+// $(document).ready(function(){
+//     $('#nightContainer > div').hover(function() {
+//         $(this).css("cursor", "pointer");
+//         $(this).toggle({
+//           effect: "scale",
+//           percent: "90%"
+//         },200);
+//     }, function() {
+//          $(this).toggle({
+//            effect: "scale",
+//            percent: "80%"
+//          },200);
+
+//     });
+// });  
+
+// $(document).ready(function(){
+//     $('#nightContainer > div').hover(function() {
+//         $("#nightContainer > div").addClass('transition');
+
+//     }, function() {
+//         $("#nightContainer > div").removeClass('transition');
+//     });
+// });
+$('#nightContainer > img').live("mouseover", function(){
+          var $this=$(this);
+
+          $this.attr('width','25%');
+          $this.attr('height','25%');
+
+
+       })
+
